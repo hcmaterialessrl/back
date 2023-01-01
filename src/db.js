09e43febcb1e632cc6c99519a06e92036df75352
JuanjoCieri <juin@hotmail.com.ar>
2023-01-01 15:38:43 -0300

    fix

--- src/db.js
+++ src/db.js
@@ -8,11 +8,6 @@ import {
 } from "./config.js"
 
 export const pool = createPool({
-    // host: "localhost",
-    // user: "id20063422_hugocieri",
-    // password: "Ptu!5Y?HbE8(Cl*l",
-    // port: 21,
-    // database: "id20063422_hcmaterialessrl"
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
